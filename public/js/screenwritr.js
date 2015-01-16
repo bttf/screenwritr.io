75594f344a5875b957186a18230c0974c6efb1e0
bttf <adnanchowdhury88@gmail.com>
2015-01-16 11:35:49 -0500

    refined parallax

--- public/js/screenwritr.js
+++ public/js/screenwritr.js
@@ -2,9 +2,15 @@
 
   $(window).scroll(function() {
     var scrollPos = $(this).scrollTop();
+
     $('.hero').css({
-      'background-position': 'center ' + (scrollPos/2) + 'px'
+      'background-position': 'center ' + (scrollPos / 2) + 'px'
     });
+
+    $('.banner-text').css({
+      'margin-top': (-scrollPos * 1.25) + 'px'
+    });
+
   });
 
 })();
